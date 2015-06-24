023997c92f3b8bf53f957ee039abd8a37e949462
root <root@localhost>
2015-06-24 16:00:19 +0200

    Add license to code: GPL-3

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,10 @@
 //!
 //!
 //!
+//! ## License
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of GPL-3.
 
 extern crate arrayvec;
 #[cfg(test)]
